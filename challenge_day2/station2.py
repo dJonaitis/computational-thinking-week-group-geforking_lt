b258c87a7c0ab8372835afdeca775f04f34f7d7d
fleming ziehm <flemingziehm@re-byods-146-50-227-240.wireless.uva.nl>
2024-09-03 10:32:32 +0200

    Station 2 and 4

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,3 +1,17 @@
-def solution_station_2(input):
-    pass
-    return
+import datetime
+import locale
+
+def solution_station_2():
+    # Set the locale to Japanese
+    locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+
+    # Example date
+    date = datetime.datetime(2024, 9, 2)  # Year, Month, Day
+
+    # Get the day of the week in Japanese
+    day_name_japanese = date.strftime("%A")
+
+    return day_name_japanese
+
+# Example usage
+print(solution_station_2())
