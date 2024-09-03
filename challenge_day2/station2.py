898236c71c68e159d28a7dcff6da3a33ae752053
NeklanV2 <fleming.ziehm@web.de>
2024-09-03 14:29:04 +0200

    Station 2

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,17 +1,21 @@
 import datetime
-import locale
 
-def solution_station_2():
-    # Set the locale to Japanese
-    locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+def solution_station_2(calenderday):
+    # Mapping Japanese weekdays in case Japanese is not preinstalled on the device
+    days_in_japanese = {
+        0: '月曜日',  # Monday
+        1: '火曜日',  # Tuesday
+        2: '水曜日',  # Wednesday
+        3: '木曜日',  # Thursday
+        4: '金曜日',  # Friday
+        5: '土曜日',  # Saturday
+        6: '日曜日'   # Sunday
+    }
 
-    # Example date
-    date = datetime.datetime(2024, 9, 2)  # Year, Month, Day
+    # Get the day of the week as an integer (0=Monday, 6=Sunday)
+    day_of_week = calenderday.weekday()
 
-    # Get the day of the week in Japanese
-    day_name_japanese = date.strftime("%A")
+    # Get the Japanese name for the day
+    day_name_japanese = days_in_japanese[day_of_week]
 
     return day_name_japanese
-
-# Example usage
-print(solution_station_2())
