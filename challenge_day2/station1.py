ba2fef25f01c290b14f90af0c468589481ef3d39
dJonaitis <55477640+dJonaitis@users.noreply.github.com>
2024-09-03 10:20:53 +0200

    Station 1 solution

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,3 +1,7 @@
 def solution_station_1(input):
-    pass
-    return
+    # Fibonacci sequence
+    n = int(input)
+    fib = [0, 1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[n]
